2dafbad5e2c041962f91e6a32a92cc904cc1a9cb
Ralph Kamogelo Chiloane <kamogeloralph@gmail.com>
2026-08-22 11:39:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
             sync, periodicsync, beforeevicted, controllerchange-friendly skipWaiting
    ========================================================================== */
 
-const VERSION = 'sa-recruiters-v117';
+const VERSION = 'sa-recruiters-v118';
 const CORE_CACHE = VERSION + '-core';
 const RUNTIME_CACHE = VERSION + '-runtime';
 const IMAGE_CACHE = VERSION + '-images';
